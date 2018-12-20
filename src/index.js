40880ada35f6a453dc18bb41ac3769340277f576
Eugenia Leung <eugenia_leung316@yahoo.com>
2018-12-20 15:46:07 -0800

    added form

--- src/index.js
+++ src/index.js
@@ -2,33 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import promise from "redux-promise";
 
-import "./index.css";
-import App from "./components/App";
-import reducers from "./reducers";
-import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
+import "./index.css";
+import reducers from "./reducers";
+import PostsIndex from "./components/posts_index";
+import PostsNew from "./components/posts_new";
+import * as serviceWorker from "./serviceWorker";
 
-const createStoreWithMiddleware = applyMiddleware(createStore);
-
-class Hello extends React.Component {
-  render() {
-    return <div>Hello!</div>;
-  }
-}
-
-class Goodbye extends React.Component {
-  render() {
-    return <div>Bye!</div>;
-  }
-}
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
-      <Route path="/hello" component={Hello} />
-      <Route path="goodbye" component={Goodbye} />
+      <div>
+        <Switch>
+          <Route path="/posts/new" component={PostsNew} />
+          <Route path="/" component={PostsIndex} />
+        </Switch>
+      </div>
     </BrowserRouter>
   </Provider>,
   document.getElementById("root")
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,18 @@
-import { combineReducers } from "redux";
+// import { combineReducers } from "redux";
 
-const rootReducer = combineReducers({});
+// const rootReducer = combineReducers({
+//   state: (state = {}) => state
+// });
+
+// export default rootReducer;
+
+import { combineReducers } from "redux";
+import { reducer as formReducer } from "redux-form";
+import PostsReducer from "./reducer_posts";
+
+const rootReducer = combineReducers({
+  posts: PostsReducer,
+  form: formReducer
+});
 
 export default rootReducer;
